dccec7d28416d43a4d6a4ce29417297cdd7f0f3c
Ganesh-krish <dhavamani.v@anjaconline.org>
2026-09-24 19:22:05 +0530

    middelware created

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,8 @@ Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::get('/auth/me',[AuthController::class,'me'])->middleware('auth:api');
 Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+Route::get('/admin/test',function(){
+    return response()->json([
+        'message' => 'Welcome Admin'
+    ]);
+})->middleware(['auth:api','role:ADMIN']);
