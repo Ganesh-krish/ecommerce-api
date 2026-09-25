bf1077160bdaf77299db1326444b23e50b08162b
Ganesh-krish <dhavamani.v@anjaconline.org>
2026-09-25 13:54:08 +0530

    fix: api routes

--- routes/api.php
+++ routes/api.php
@@ -16,21 +16,22 @@ Route::apiResource(
     CategoryController::class
 )
     ->middleware('auth:api')
-    ->middlewareFor(['index','show'],'permission:product.view')
-    ->middlewareFor('store', 'permission:products.create')
-    ->middlewareFor('update', 'permission:products.update')
-    ->middlewareFor('destroy', 'permission:products.delete');
+    ->middlewareFor(['index', 'show'], 'permission:categories.view')
+    ->middlewareFor('store', 'permission:categories.create')
+    ->middlewareFor('update', 'permission:categories.update')
+    ->middlewareFor('destroy', 'permission:categories.delete');
+
 
 Route::apiResource(
     'products',
     ProductController::class
 )
     ->middleware('auth:api')
-    ->middlewareFor(['index', 'show'], 'permission:categories.view')
-    ->middlewareFor('store', 'permission:categories.create')
-    ->middlewareFor('update', 'permission:categories.update')
-        ->middlewareFor('destroy', 'permission:categories.delete');
-
+    ->middlewareFor(['index', 'show'], 'permission:products.view')
+    ->middlewareFor('store', 'permission:products.create')
+    ->middlewareFor('update', 'permission:products.update')
+    ->middlewareFor('destroy', 'permission:products.delete');
+    
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
